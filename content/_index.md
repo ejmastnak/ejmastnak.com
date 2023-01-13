7b42a086e0f00c2d7db95084b22a3fb8127303a0
Elijan Mastnak <ejmastnak@gmail.com>
2023-01-13 11:36:27 +0100

    style: add inline code styling

--- content/_index.md
+++ content/_index.md
@@ -8,26 +8,26 @@ For the time being, you can either view my [résumé](/resume.pdf) or read throu
 
 ### Tutorials
 
-[<u>**Real-time LaTeX using (Neo)Vim**</u>]({{< relref "/tutorials/vim-latex/intro.md" >}})
+[**Real-time LaTeX using (Neo)Vim**]({{< relref "/tutorials/vim-latex/intro.md" >}})
 <br>
 A guide to turbocharged mathematical typesetting
 
-[<u>**Find your footing on Arch Linux**</u>]({{< relref "/tutorials/arch/about" >}})
+[**Find your footing on Arch Linux**]({{< relref "/tutorials/arch/about" >}})
 <br>
 Bite-sized tutorials to help you set up a functional work environment after a minimal install of Arch
 
 ### Notes
 
-[<u>**1500+ pages of undergraduate physics notes typeset with LaTeX**</u>]({{< relref "/notes/fmf/fmf" >}})
+[**1500+ pages of undergraduate physics notes typeset with LaTeX**]({{< relref "/notes/fmf/fmf" >}})
 <br>
 From the final two years of undergrad at the Faculty of Math and Physics in Ljubljana
 
 ### Projects
 
-[<u>**Undergraduate "mini-thesis"**</u>]({{< relref "/projects/seminar" >}})
+[**Undergraduate "mini-thesis"**]({{< relref "/projects/seminar" >}})
 <br>
 End-to-end particle physics classification with convolutional neural networks
 
-[<u>**Digital Hilbert transformer "from scratch"**</u>]({{< relref "/projects/hilbert" >}})
+[**Digital Hilbert transformer "from scratch"**]({{< relref "/projects/hilbert" >}})
 <br>
 A (near) real-time FIR Hilbert transformer and bandpass filter using Python's Numpy and PyAudio
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.800'),
+            'code::before': {
+              content: '',
+            },
+            'code::after': {
+              content: '',
+            },
+            code: {
+              backgroundColor: theme('colors.gray.200'),
+              borderRadius: '4px',
+              padding: '.2rem .4rem',
+              fontWeight: 'normal',
+              fontSize: '85%',
+            },
             a: {
               textDecoration: 'none',
               color: theme('colors.sky.600'),
@@ -28,3 +40,79 @@ module.exports = {
   ]
 }
 
+// a code {
+//   padding: .2em .4em;
+//   margin: 0;
+//   font-size: 85%;
+//   background-color: $inline-code-background-color;
+//   color: $link-color;
+//   border-radius: 3px;
+// }
+
+
+// // Element: code, highlight, pre, etc
+// // -----------------------------------------------
+// code,
+// pre {
+//   font-family: 'Computer Modern Typewriter',monospace;
+// }
+//
+// pre {
+//   margin-top: 0;
+//   margin-bottom: 0;
+//   word-wrap: normal;
+// }
+//
+// // Note: Text color and background color of inline code
+// code {
+//   padding: .2em .4em;
+//   margin: 0;
+//   font-size: 85%;
+//   background-color: $inline-code-background-color;
+//   color: $inline-code-text-color;
+//   border-radius: 3px;
+// }
+//
+// pre>code {
+//   padding: 0;
+//   margin: 0;
+//   font-size: 100%;
+//   word-break: normal;
+//   white-space: pre;
+//   background: transparent;
+//   border: 0;
+// }
+//
+// .highlight {
+//   margin-bottom: 16px;
+// }
+//
+// .highlight pre {
+//   margin-bottom: 0;
+//   word-break: normal;
+// }
+//
+// // Note: Background color of code blocks
+// .highlight pre,
+// pre {
+//   padding: 16px;
+//   overflow: auto;
+//   font-size: 90%;
+//   line-height: 1.45;
+//   background-color: $block-code-background-color;
+//   border-radius: 3px;
+// }
+//
+// // Note: Text color and background color inside code blocks
+// pre code {
+//   display: inline;
+//   max-width: auto;
+//   padding: 0;
+//   margin: 0;
+//   overflow: visible;
+//   line-height: inherit;
+//   word-wrap: normal;
+//   background-color: initial;
+//   color: $block-code-text-color;
+//   border: 0;
+// }

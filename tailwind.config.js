ce63757d5c143686d368f002841e4d41220b4c66
Elijan Mastnak <ejmastnak@gmail.com>
2023-01-24 09:17:06 +0100

    feat: add some initial dark mode experimentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  darkMode: 'class',
   content: [
     "./content/**/*.md",  // for example
     "./layouts/**/*.html",
@@ -13,20 +14,50 @@ module.exports = {
         'sans': ['Inter', ...defaultTheme.fontFamily.sans],
       },
       typography: (theme) => ({
-        DEFAULT: {
+        ejmastnak: {  // my custom typography theme; slight fork of default gray theme 
           css: {
+            '--tw-prose-body': theme('colors.gray[700]'),
+            '--tw-prose-headings': theme('colors.gray[900]'),
+            '--tw-prose-lead': theme('colors.gray[600]'),
+            '--tw-prose-links': theme('colors.sky[600]'),
+            '--tw-prose-bold': theme('colors.gray[900]'),
+            '--tw-prose-counters': theme('colors.gray[500]'),
+            '--tw-prose-bullets': theme('colors.gray[400]'),
+            '--tw-prose-hr': theme('colors.gray[200]'),
+            '--tw-prose-quotes': theme('colors.gray[900]'),
+            '--tw-prose-quote-borders': theme('colors.gray[200]'),
+            '--tw-prose-captions': theme('colors.gray[500]'),
+            '--tw-prose-code': theme('colors.gray[900]'),
+            '--tw-prose-pre-code': theme('colors.gray[200]'),
+            '--tw-prose-pre-bg': theme('colors.gray[800]'),
+            '--tw-prose-th-borders': theme('colors.gray[300]'),
+            '--tw-prose-td-borders': theme('colors.gray[200]'),
+            '--tw-prose-invert-body': theme('colors.gray[300]'),
+            '--tw-prose-invert-headings': theme('colors.white'),
+            '--tw-prose-invert-lead': theme('colors.gray[400]'),
+            '--tw-prose-invert-links': theme('colors.white'),
+            '--tw-prose-invert-bold': theme('colors.white'),
+            '--tw-prose-invert-counters': theme('colors.gray[400]'),
+            '--tw-prose-invert-bullets': theme('colors.gray[600]'),
+            '--tw-prose-invert-hr': theme('colors.gray[700]'),
+            '--tw-prose-invert-quotes': theme('colors.gray[100]'),
+            '--tw-prose-invert-quote-borders': theme('colors.gray[700]'),
+            '--tw-prose-invert-captions': theme('colors.gray[400]'),
+            '--tw-prose-invert-code': theme('colors.white'),
+            '--tw-prose-invert-pre-code': theme('colors.gray[300]'),
+            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+            '--tw-prose-invert-th-borders': theme('colors.gray[600]'),
+            '--tw-prose-invert-td-borders': theme('colors.gray[700]'),
             h1: {
               fontSize: '2.25rem',
               lineHeight: '2.5rem',
               marginBottom: '2rem',
-              color: theme('colors.gray.800'),
               fontWeight: '600',
             },
             img: {
               marginTop: '0.5rem',
               marginBottom: '0.5rem',
             },
-            '--tw-prose-bullets': theme('colors.gray.400'),
             'code::before': {
               content: '',
             },
@@ -42,10 +73,9 @@ module.exports = {
             },
             a: {
               textDecoration: 'none',
-              color: theme('colors.sky.600'),
               '&:hover': {
                 textDecoration: 'underline',
-                color: theme('colors.sky.800'),
+                // color: theme('colors.sky.800'),
               },
             },
           },

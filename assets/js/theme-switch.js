87f919de5ef123a3cb9f271c774500fd3725908f
Elijan Mastnak <git@ejmastnak.com>
2023-09-29 12:04:07 +0200

    refactor: clean up theme switch script
    Resolves commit 949c8002e02354b3364457f0329be3f137c03a6d, continues with
    adding themeswitch prefix to variables and ids, adds moveToNext|Prev
    methods to abstract moving logic, and adds event listeners for Tab and
    Shift-Tab.

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -4,18 +4,18 @@ var themeswitchListboxButtonSunIcon = document.getElementById('themeswitch-listb
 var themeswitchListboxButtonMoonIcon = document.getElementById('themeswitch-listbox-button-moon');
 
 var themeswitchOptions = [...themeswitchListbox.children];
-var lightOption = document.getElementById('listbox-option-light');
-var lightIcon = document.getElementById('light-icon');
-var lightLabel = document.getElementById('light-label');
-var darkOption = document.getElementById('listbox-option-dark');
-var darkIcon = document.getElementById('dark-icon');
-var darkLabel = document.getElementById('dark-label');
-var systemOption = document.getElementById('listbox-option-system');
-var systemIcon = document.getElementById('system-icon');
-var systemLabel = document.getElementById('system-label');
+var themeswitchLightOption = document.getElementById('themeswitch-light-option');
+var themeswitchLightIcon = document.getElementById('themeswitch-light-icon');
+var themeswitchLightLabel = document.getElementById('themeswitch-light-label');
+var themeswitchDarkOption = document.getElementById('themeswitch-dark-option');
+var themeswitchDarkIcon = document.getElementById('themeswitch-dark-icon');
+var themeswitchDarkLabel = document.getElementById('themeswitch-dark-label');
+var themeswitchSystemOption = document.getElementById('themeswitch-system-option');
+var themeswitchSystemIcon = document.getElementById('themeswitch-system-icon');
+var themeswitchSystemLabel = document.getElementById('themeswitch-system-label');
 
 // For the purposes of keyboard navigation through listbox options
-var themeswitchActiveOption = systemOption;
+var themeswitchActiveOption = themeswitchSystemOption;
 
 const themeswitchColorActive = 'text-sky-500';
 const themeswitchColorActiveDark = 'dark:text-sky-500';
@@ -46,66 +46,62 @@ function showListbox() {
 
 function toggleListbox() {
   const expanded = themeswitchListboxButton.getAttribute('aria-expanded');
-  if (expanded === 'true') {
-    hideListbox();
-  } else {
-    showListbox();
-  }
+  expanded === 'true' ? hideListbox() : showListbox();
 }
 
-// Set initial option appearance/state
+// Set initial active option based on local storage
 if (localStorage.getItem('theme') === 'dark') {
   unstyleAll();
-  style('dark');
-  switchSelectionTo('dark');
-  themeswitchActiveOption = darkOption;
+  switchActiveStyleTo('dark');
+  switchSelectedAttributeTo('dark');
+  themeswitchActiveOption = themeswitchDarkOption;
 }
 else if (localStorage.getItem('theme') === 'light') {
   unstyleAll();
-  style('light');
-  switchSelectionTo('light');
-  themeswitchActiveOption = lightOption;
+  switchActiveStyleTo('light');
+  switchSelectedAttributeTo('light');
+  themeswitchActiveOption = themeswitchLightOption;
 } else {
   unstyleAll();
-  style('system');
-  switchSelectionTo('system');
-  themeswitchActiveOption = systemOption;
+  switchActiveStyleTo('system');
+  switchSelectedAttributeTo('system');
+  themeswitchActiveOption = themeswitchSystemOption;
 }
 
 // Removes selected styles from all listbox labels and icons
 function unstyleAll() {
-  lightIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
-  lightLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
-  darkIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
-  darkLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
-  systemIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
-  systemLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchLightIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchLightLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchDarkIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchDarkLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchSystemIcon.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
+  themeswitchSystemLabel.classList.remove(themeswitchColorActive, themeswitchColorActiveDark);
 }
 
 // Removes background style from all listbox options
 function removeAllBG() {
-  lightOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
-  darkOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
-  systemOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchLightOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchDarkOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchSystemOption.classList.remove(themeswitchBGActive, themeswitchBGActiveDark);
 }
 
 // Manages stylistic changes when switching themes
-function style(theme) {
+function switchActiveStyleTo(theme) {
   if (theme === 'light') {
-    lightIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
-    lightLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchLightIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchLightLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
     themeswitchListboxButtonMoonIcon.classList.add('hidden');
     themeswitchListboxButtonSunIcon.classList.remove('hidden');
     themeswitchListboxButtonSunIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
   } else if (theme === 'dark') {
-    darkIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
-    darkLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchDarkIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchDarkLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
     themeswitchListboxButtonSunIcon.classList.add('hidden');
     themeswitchListboxButtonMoonIcon.classList.remove('hidden');
     themeswitchListboxButtonMoonIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
   } else if (theme === 'system') {
-    systemIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
-    systemLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchSystemIcon.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
+    themeswitchSystemLabel.classList.add(themeswitchColorActive, themeswitchColorActiveDark);
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       themeswitchListboxButtonSunIcon.classList.add('hidden');
       themeswitchListboxButtonMoonIcon.classList.remove('hidden');
@@ -119,19 +115,19 @@ function style(theme) {
 }
 
 // Manages options' aria-selected attribute on theme switch
-function switchSelectionTo(theme) {
+function switchSelectedAttributeTo(theme) {
   if (theme === 'light') {
-    darkOption.setAttribute('aria-selected', 'false');
-    systemOption.setAttribute('aria-selected', 'false');
-    lightOption.setAttribute('aria-selected', 'true');
+    themeswitchDarkOption.setAttribute('aria-selected', 'false');
+    themeswitchSystemOption.setAttribute('aria-selected', 'false');
+    themeswitchLightOption.setAttribute('aria-selected', 'true');
   } else if (theme === 'dark') {
-    lightOption.setAttribute('aria-selected', 'false');
-    systemOption.setAttribute('aria-selected', 'false');
-    darkOption.setAttribute('aria-selected', 'true');
+    themeswitchLightOption.setAttribute('aria-selected', 'false');
+    themeswitchSystemOption.setAttribute('aria-selected', 'false');
+    themeswitchDarkOption.setAttribute('aria-selected', 'true');
   } else if (theme === 'system') {
-    lightOption.setAttribute('aria-selected', 'false');
-    darkOption.setAttribute('aria-selected', 'false');
-    systemOption.setAttribute('aria-selected', 'true');
+    themeswitchLightOption.setAttribute('aria-selected', 'false');
+    themeswitchDarkOption.setAttribute('aria-selected', 'false');
+    themeswitchSystemOption.setAttribute('aria-selected', 'true');
   }
 }
 
@@ -139,8 +135,8 @@ function switchModeToDark() {
   localStorage.setItem('theme', 'dark');
   document.documentElement.classList.add('dark');
   unstyleAll();
-  style('dark');
-  switchSelectionTo('dark');
+  switchActiveStyleTo('dark');
+  switchSelectedAttributeTo('dark');
   hideListbox();
 }
 
@@ -148,8 +144,8 @@ function switchModeToLight() {
   localStorage.setItem('theme', 'light');
   document.documentElement.classList.remove('dark');
   unstyleAll();
-  style('light');
-  switchSelectionTo('light');
+  switchActiveStyleTo('light');
+  switchSelectedAttributeTo('light');
   hideListbox();
 }
 
@@ -161,8 +157,8 @@ function switchModeToSystem() {
     document.documentElement.classList.remove('dark');
   }
   unstyleAll();
-  style('system');
-  switchSelectionTo('system');
+  switchActiveStyleTo('system');
+  switchSelectedAttributeTo('system');
   hideListbox();
 }
 
@@ -173,9 +169,9 @@ themeswitchListboxButton.addEventListener('click', function () {
 });
 
 // Switch modes when clicking options
-darkOption.addEventListener('click', switchModeToDark);
-lightOption.addEventListener('click', switchModeToLight);
-systemOption.addEventListener('click', switchModeToSystem);
+themeswitchDarkOption.addEventListener('click', switchModeToDark);
+themeswitchLightOption.addEventListener('click', switchModeToLight);
+themeswitchSystemOption.addEventListener('click', switchModeToSystem);
 
 // Hide listbox when clicking outside listbox
 document.addEventListener('mouseup', function(event) {
@@ -184,12 +180,11 @@ document.addEventListener('mouseup', function(event) {
   }
 });
 
-// We highlight bg of selected option when opening listbox
-// but then switch to mouse-hover-based bg highlighting.
-themeswitchOptions.forEach(element => element.addEventListener('mouseenter', function(event) {
+// Highlight option on mouse hover
+themeswitchOptions.forEach(option => option.addEventListener('mouseenter', function(event) {
   removeAllBG();
-  element.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
-  themeswitchActiveOption = element;
+  option.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchActiveOption = option;
 }));
 themeswitchListbox.addEventListener('mouseleave', function(event) {
   removeAllBG();
@@ -201,20 +196,20 @@ themeswitchListbox.addEventListener('keydown', function(event) {
     hideListbox();
   } else if (event.key === "ArrowDown") {
     event.preventDefault();   // prevent page from scrolling down
-    movedToOption = nextOption(themeswitchActiveOption);
-    removeAllBG();
-    movedToOption.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
-    themeswitchActiveOption = movedToOption;
+    moveToNextThemeswitchOption();
   } else if (event.key === "ArrowUp") {
     event.preventDefault();   // prevent page from scrolling up
-    movedToOption = prevOption(themeswitchActiveOption);
-    removeAllBG();
-    movedToOption.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
-    themeswitchActiveOption = movedToOption;
+    moveToPrevThemeswitchOption();
+  } else if (event.shiftKey && event.key == "Tab") {
+    event.preventDefault();   // trap focus in menu
+    moveToPrevThemeswitchOption();
+  } else if (event.key == "Tab") {
+    event.preventDefault();   // trap focus in menu
+    moveToNextThemeswitchOption();
   } else if (event.key === 'Enter') {
-    if (themeswitchActiveOption == lightOption) {
+    if (themeswitchActiveOption == themeswitchLightOption) {
       switchModeToLight();
-    } else if (themeswitchActiveOption == darkOption) {
+    } else if (themeswitchActiveOption == themeswitchDarkOption) {
       switchModeToDark();
     } else {
       switchModeToSystem();
@@ -222,22 +217,42 @@ themeswitchListbox.addEventListener('keydown', function(event) {
   }
 });
 
-function nextOption(option) {
-  if (option == lightOption) {
-    return darkOption;
-  } else if (option == darkOption) {
-    return systemOption;
+// Used with arrow key and tab navigation to move selection to next option
+function moveToNextThemeswitchOption() {
+  movedToOption = nextOption();
+  removeAllBG();
+  movedToOption.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchActiveOption = movedToOption;
+}
+
+// Used with arrow key and tab navigation to move selection to previous option
+function moveToPrevThemeswitchOption() {
+  movedToOption = prevOption();
+  removeAllBG();
+  movedToOption.classList.add(themeswitchBGActive, themeswitchBGActiveDark);
+  themeswitchActiveOption = movedToOption;
+}
+
+// Returns the option after currently active option; hardcoding seems simpler
+// than looping over options in this case, since options never change.
+function nextOption() {
+  if (themeswitchActiveOption == themeswitchLightOption) {
+    return themeswitchDarkOption;
+  } else if (themeswitchActiveOption == themeswitchDarkOption) {
+    return themeswitchSystemOption;
   } else {
-    return lightOption;
+    return themeswitchLightOption;
   }
 }
 
-function prevOption(option) {
-  if (option == lightOption) {
-    return systemOption;
-  } else if (option == darkOption) {
-    return lightOption;
+// Returns the option before currently active option; hardcoding seems simpler
+// than looping over options in this case, since options never change.
+function prevOption() {
+  if (themeswitchActiveOption == themeswitchLightOption) {
+    return themeswitchSystemOption;
+  } else if (themeswitchActiveOption == themeswitchDarkOption) {
+    return themeswitchLightOption;
   } else {
-    return darkOption;
+    return themeswitchDarkOption;
   }
 }
